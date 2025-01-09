ac4e7840e06bf95ac8c4bc054bf802ad06a11fb0
John Doe <aloysius7m@gmail.com>
2025-01-09 16:07:14 +0800

    trial

--- code.js
+++ code.js
@@ -7,3 +7,23 @@ function bigImg(x) {
     x.style.height = "35px";
     x.style.width = "35px";
   }
+
+  let burgerBtn = document.querySelector(".burger-menu-btn");
+  let burgerMenu = document.querySelector(".burger-menu");
+  let trial = document.querySelector(".abouttext1");
+
+  let isBurgerOpen = false;
+
+  burgerBtn.onclick = function() {
+    trial.style.display = "none";
+    if (!isBurgerOpen) {
+      burgerMenu.style.display = "block";
+      burgerBtn.style.backgroundPosition = "center left 50px, center";
+      isBurgerOpen = true;
+    }
+    else if (isBurgerOpen) {
+      burgerMenu.style.display = "none";
+      burgerBtn.style.backgroundPosition = "center, center left 50px";
+      isBurgerOpen = false;
+    }
+  }
